e4442d094d37f547119af212aa5fff441c08efd0
Michael Le <michaelle@Michaels-MacBook-Pro.local>
2018-11-06 18:04:14 -0500

    add js for buttons

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,21 @@
 (function () {
+  // get buttons
+  const buttons = document.querySelectorAll('button');
+  const buttonOne = buttons[0];
+  const buttonTwo = buttons[1];
+  const buttonThree = buttons[2];
 
-  // your js goes here
+  // listen for clicks on the document
+  buttonOne.addEventListener('click', function() {
+    buttonOne.classList.toggle('button-danger');
+  });
+
+  buttonTwo.addEventListener('click', function() {
+    buttonTwo.innerText = 'You Clicked Me!';
+  });
+
+  buttonThree.addEventListener('click', function() {
+    buttonTwo.innerText = 'button Two';
+  });
 
 })();
